cc797b4dab722381bb39ce45b8d70434851f06e1
Darragh Bailey <daragh.bailey@gmail.com>
2022-02-24 16:50:39 +0000

    inline should pass args directly

--- boxes.rb
+++ boxes.rb
@@ -82,7 +82,7 @@ BOXES = {
 }
 
 DEFAULT_PROVISION = [
-  {:privileged => false, :inline => '/vagrant/scripts/install.bash', :args => QA_VAGRANT_VERSION, :env => INSTALL_ENV_VARS},
+  {:privileged => false, :inline => "/vagrant/scripts/install.bash #{QA_VAGRANT_VERSION}", :env => INSTALL_ENV_VARS},
   {:reset => true, :inline => 'usermod -a -G libvirt vagrant'},
   {:privileged => false, :inline => 'virsh --connect qemu:///system capabilities'},
   {:privileged => false, :inline => 'virsh uri'},
